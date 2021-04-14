a26d137cab7af461735b5a7dfbb4453b9f924a96
Sam Mudick <smudick@gmail.com>
2021-04-13 20:15:39 -0500

    Read - get all order_details is working

---
+++ Spinnovations/Controllers/Order_DetailsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spinnovations.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spinnovations.Controllers
+{
+    [Route("api/Order_Details")]
+    [ApiController]
+    public class Order_DetailsController : ControllerBase
+    {
+        Order_DetailsRepository _repo;
+        public Order_DetailsController(Order_DetailsRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public IActionResult GetAllOrder_Details()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+    }
+}
---
+++ Spinnovations/Data/Order_DetailsRepository.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Spinnovations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spinnovations.Data
+{
+    public class Order_DetailsRepository
+    {
+        readonly string ConnectionString;
+        public Order_DetailsRepository(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("Spinnovations");
+        }
+        public List<Order_Details> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+            var sql = "SELECT * FROM Order_Details";
+            return db.Query<Order_Details>(sql).ToList();
+        }
+    }
+}
