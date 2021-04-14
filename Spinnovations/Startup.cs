906792c4ce8d4a7aa90525cbdf53ed35c599221e
Michael Pfohl <rmpfohl@gmail.com>
2021-04-13 20:02:05 -0500

    crud complete for product API

--- Spinnovations/Startup.cs
+++ Spinnovations/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Spinnovations.Data;
 
 namespace Spinnovations
 {
@@ -26,6 +27,8 @@ namespace Spinnovations
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton(Configuration);
+            services.AddTransient<ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
