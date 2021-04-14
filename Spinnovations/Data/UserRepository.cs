390f0c5ade021acd30cd988cf7eba321af3c7bee
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-04-13 20:18:25 -0500

    created user class

---
+++ Spinnovations/Data/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Spinnovations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Spinnovations.Data
+{
+    public class UserRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=Spinnovations;Trusted_Connection=True;";
+
+        //public List<User> GetAll()
+        //{
+            //using var db = new SqlConnection(ConnectionString);
+            //var sql = @"SELECT *
+            //            FROM Users";
+            //return db.Query<User>(sql).ToList();
+        //}
+    }
+}
---
+++ Spinnovations/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spinnovations.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Postal_Code { get; set; }
+        public int Payment_Info_Id { get; set; }
+        public string Display_Name { get; set; }
+        public string Profile_Picture { get; set; }
+        public DateTime User_Created_Date { get; set; }
+    }
+}
