d664407ac27fd9bfbd16e89e2d4f6d7c03dcbd4b
Bailey <bailey.elise.dennis@gmail.com>
2021-04-13 20:21:34 -0500

    payment information skeleton built

---
+++ Spinnovations/Controllers/PaymentInfoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Spinnovations.Data;
+
+namespace Spinnovations.Controllers
+{
+    [ApiController]
+    [Route("api/paymentInfo")]
+    public class PaymentInfoController : ControllerBase
+    {
+        PaymentInfoRepository _repo;
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_repo.GetAll());
+        }
+    }
+}
---
+++ Spinnovations/Data/PaymentInfoRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Spinnovations.Models;
+
+namespace Spinnovations.Data
+{
+    public class PaymentInfoRepository
+    {
+        const string ConnectionString = "placeholder";
+
+        public List<Product> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+            var sql = "SELECT * FROM PaymentInfo";
+            return db.Query<Product>(sql).ToList();
+        }
+    }
+}
