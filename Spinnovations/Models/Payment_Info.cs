f4457416c8b590e17de629d4d75ae27bccb7fcfe
Bailey <bailey.elise.dennis@gmail.com>
2021-04-15 20:25:37 -0500

    CRUD for a users payment information now works!

--- Spinnovations/Models/Payment_Info.cs
+++ Spinnovations/Models/Payment_Info.cs
@@ -9,6 +9,7 @@ namespace Spinnovations.Models
     public class Payment_Info
     {
         public int Id { get; set; }
+        public Card Card_Company { get; set; }
         public string Card_Number { get; set; }
         public Month Expiration_Month { get; set; }
         public int Expiration_Year { get; set; }
