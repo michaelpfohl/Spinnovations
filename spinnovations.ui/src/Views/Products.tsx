506d241106985ea27e7d4a7631f89284fa07e336
Michael Pfohl <rmpfohl@gmail.com>
2021-04-20 21:30:07 -0500

    display product titles with api call to db

---
+++ spinnovations.ui/src/Views/Products.tsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import productData, { Product } from '../Helpers/Data/ProductData';
+
+class Products extends Component {
+    state = {
+        products: [],
+    };
+
+    componentDidMount(): void {
+        productData.getProducts().then((response : Product[]) => {
+            this.setState({
+                products: response
+            })
+        });
+    }
+    render() : JSX.Element {
+        const { products } = this.state
+        const productCard = (product: Product) => {
+            return (
+                <div key={product.id}>
+                    <h1>{product.name}</h1>
+                </div>
+            )
+        }
+
+        console.log("products", products)
+        const cards = products.map(productCard)
+        return (
+            <div>
+                {cards}
+            </div>
+        )
+    }
+}
+
+export default Products;
