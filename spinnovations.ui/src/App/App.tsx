f036ad55cdb6dba942eb2fa5b5cbed625c418502
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-04-24 11:37:03 -0500

    reactstrap and navbar

--- spinnovations.ui/src/App/App.tsx
+++ spinnovations.ui/src/App/App.tsx
@@ -2,12 +2,15 @@ import React, { ReactElement } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
 import Routes from '../Helpers/routes';
+import Navigation from '../Components/Navbar';
 
 const App: React.FC = (): ReactElement => {
   return (
     <div className="App">
       <Router>
-        <Routes />
+        <Navigation>
+          <Routes />
+        </Navigation>
       </Router>
     </div>
   );
