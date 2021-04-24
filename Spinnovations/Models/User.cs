2c21c0256c390fb4e0bf64242efc56a5cf0671b4
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-04-24 09:58:05 -0500

    updated getdate, removed package.lock and removed payment info id

--- Spinnovations/Models/User.cs
+++ Spinnovations/Models/User.cs
@@ -14,7 +14,6 @@ namespace Spinnovations.Models
         public string City { get; set; }
         public string Country { get; set; }
         public string Postal_Code { get; set; }
-        public int Payment_Info_Id { get; set; }
         public string Display_Name { get; set; }
         public string Profile_Picture { get; set; }
         public DateTime User_Created_Date { get; set; }
