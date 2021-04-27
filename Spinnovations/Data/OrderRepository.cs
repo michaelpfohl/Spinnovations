03614d6a562402c30fe09121cdd01b53de9e4ecd
Sam Mudick <smudick@gmail.com>
2021-04-27 17:24:23 -0500

    still working on getting full order data

--- Spinnovations/Data/OrderRepository.cs
+++ Spinnovations/Data/OrderRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Spinnovations.Models;
+using Spinnovations.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,19 +34,56 @@ namespace Spinnovations.Data
         public IEnumerable<Order> GetAllOrdersByUser(int customerId)
         {
             using var db = new SqlConnection(ConnectionString);
-            var sql = $@"SELECT * from orders o
-                            JOIN Order_Details od
-                                ON od.Order_Id = o.Id
-                            JOIN Products p 
-                                On p.id = od.Product_Id
-                          WHERE o.Customer_Id = @customerId";
-            var userOrders = db.Query<Order, List<Order_Details>, List<Product>, Order>(sql, (order, order_details, product) =>
+            //var sql = $@"SELECT * from Orders o
+            //                JOIN Order_Details od 
+            //                    ON od.Order_Id = o.id
+            //                JOIN Products p
+            //                    ON p.id = od.Product_Id
+            //                WHERE o.Customer_Id = 4";
+            //var userOrders = db.Query<Order, Order_Details, Product, Order>(sql, (order, order_details, product) =>
+            //{
+            //    order.Order_Details = new List<Order_Details>();
+            //    order.Products = new List<Product>();
+            //    order.Order_Details.Add(order_details);
+            //    order.Products.Add(product);
+            //    return order;
+            //}, new { customerId = customerId }, splitOn: "Id");
+            //return userOrders;
+            var orderSql = $@"SELECT * from Orders o
+                            WHERE o.Customer_Id = @customerId";
+            var productSql = $@"SELECT * from Products p
+                                WHERE p.Id = @productId";
+            var orderDetailSql = $@"SELECT * from Order_Details
+                         WHERE Order_Id = @orderId";
+
+            var productIds = new List<int>();
+            var orderIds = new List<int>();
+            var orderDetails = new List<Order_Details>();
+            var products = new List<Product>();
+
+            var userOrders = db.Query<Order>(orderSql, new { customerId = customerId });
+
+            foreach (var userOrder in userOrders)
             {
-                order.Order_Details = order_details;
-                order.Products = product;
-                return order;
-            }, new { customerId = customerId }, splitOn:"Id");
-            return userOrders;
+                orderIds.Add(userOrder.Id);
+            }
+
+            foreach (var orderId in orderIds)
+            {
+                orderDetails = db.Query<Order_Details>(orderDetailSql, new { orderId = orderId }).ToList();
+            }
+
+            foreach (var orderDetail in orderDetails)
+            {
+                productIds.Add(orderDetail.Product_Id);
+            }
+
+            foreach (var productId in productIds)
+            {
+                products = db.Query<Product>(productSql, new { productId = productId }).ToList();
+            }
+
+            
         }
         public void Add(Order order)
         {
--- Spinnovations/Data/Order_DetailsRepository.cs
+++ Spinnovations/Data/Order_DetailsRepository.cs
@@ -63,12 +63,12 @@ namespace Spinnovations.Data
                         WHERE Id = @id";
             db.Execute(sql, orderDetails);
         }
-        public void GetOrderDetailsByOrder(int orderId)
+        public IEnumerable<Order_Details> GetOrderDetailsByOrder(int orderId)
         {
             using var db = new SqlConnection(ConnectionString);
             var sql = $@"SELECT * from Order_Details
                          WHERE Order_Id = @orderId";
-            db.Execute(sql, new { orderId = orderId });
+            return db.Query<Order_Details>(sql, new { orderId = orderId });
         }
     }
 }
