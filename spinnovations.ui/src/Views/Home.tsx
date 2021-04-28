88df76f87c45d03039aabde79a57e261e3a0733b
Kaitlyn VanHook <kaitlyn.vanhook@gmail.com>
2021-04-27 20:05:16 -0500

    last twenty products displaying on home page

--- spinnovations.ui/src/Views/Home.tsx
+++ spinnovations.ui/src/Views/Home.tsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import { ProductCard } from '../Components/Cards/ProductCard';
 import HomeCarousel from '../Components/Carousel/HomeCarousel';
+import productData from '../Helpers/Data/ProductData';
+import { Product } from '../Helpers/Interfaces/ProductInterfaces';
+
+type HomeState = {
+  products?: Product[],
+}
 
 class Home extends Component {
+  
+  state: HomeState = {
+    products: []
+  };
+
+  componentDidMount(): void {
+    productData.getLastTwentyProducts().then((response: Product[]) => {
+      this.setState({
+        products: response
+      })
+    })
+  }
+
   render() : JSX.Element {
+    const { products } = this.state;
+    const productCard = (product: Product): JSX.Element => {
+      return (<ProductCard product={product} />)
+    };
+    let cards = products?.map(productCard);
       return (
           <div>
               <HomeCarousel />
-              <h1>Home Under Construction!</h1>
+              <div className="d-flex flex-wrap">
+                  {cards}
+              </div>
           </div>
       )
   }
