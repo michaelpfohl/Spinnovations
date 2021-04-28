d81afec632560b7561ef28acf2d6953e8367a07f
Sam Mudick <smudick@gmail.com>
2021-04-27 21:00:03 -0500

    save

--- spinnovations.ui/src/Components/Cards/OrderCard.tsx
+++ spinnovations.ui/src/Components/Cards/OrderCard.tsx
@@ -1,17 +1,19 @@
-import { OrderDetails, OrderProps } from '../../Helpers/Interfaces/OrderInterfaces';
+import { OrderDetails, OrderProps, Order } from '../../Helpers/Interfaces/OrderInterfaces';
 
-const calcOrderTotal = ({ order }: OrderProps): number => {
+const calcOrderTotal = ({ order }: Order): number => {
     let totalCost = 0;
+    console.log(order);
     order.order_Details.forEach(function (detail: OrderDetails) {
-        totalCost += detail.unit_Price 
+        totalCost += detail.unit_Price; 
     });
     return totalCost;
 }
 
 export const OrderCard = ({ order }: OrderProps): JSX.Element => 
-    <div className="order-card" key={order.id}>
+    <div className="order-card" key={Math.random()+order.id}>
+        {console.log(order)}
         <p>ORDER DATE: {order.order_Date}</p>
-        <p>TOTAL AMOUNT: ${order.order_Details[0].unit_Price}</p>
+        {/* <p>TOTAL AMOUNT: ${calcOrderTotal(order)}</p> */}
         <p>ADDRESS: {order.address}, {order.city} {order.country} {order.postal_code}</p>
         <p>PRODUCT: {order.products[0].name}</p>
     </div>
