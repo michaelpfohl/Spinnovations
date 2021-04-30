049ca7102be75c5e661fd10e9dbd4af612774f65
Sam Mudick <smudick@gmail.com>
2021-04-29 21:05:48 -0500

    save

--- spinnovations.ui/src/Components/Cards/OrderCard.tsx
+++ spinnovations.ui/src/Components/Cards/OrderCard.tsx
@@ -9,52 +9,15 @@ const calcOrderTotal = ({ order }: Order): number => {
     return totalCost;
 }
 
- => 
-    <div className="order-card" key={Math.random()+order.id}>
-        {console.log(order)}
-        <p>ORDER DATE: {order.order_Date}</p>
-        {/* <p>TOTAL AMOUNT: ${calcOrderTotal(order)}</p> */}
-        <p>ADDRESS: {order.address}, {order.city} {order.country} {order.postal_code}</p>
-        <p>PRODUCT: {order.products[0].name}</p>
-    </div>
-
-
-import React from 'react';
-import { Table } from 'reactstrap';
-
 export const OrderCard = ({ order }: OrderProps): JSX.Element => {
   return (
-    <Table hover>
-      <thead>
+    <div className="order-row" key={Math.random()+order.id}>
         <tr>
-          <th>#</th>
-          <th>Order Date</th>
-          <th>Price</th>
-          <th>Username</th>
+          <th scope='row'></th>
+          <td>{order.order_Date}</td>
+          <td>{order.address}, {order.city} {order.country} {order.postal_code}</td>
+          <td>$$$</td>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
+    </div>
   );
 }
-
-export default Example;
--- spinnovations.ui/src/Views/Orders.tsx
+++ spinnovations.ui/src/Views/Orders.tsx
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
-import { Order } from '../Helpers/Interfaces/OrderInterfaces';
-import orderData from '../Helpers/Data/orderData';
-import { OrderCard } from '../Components/Cards/OrderCard';
+import React, { Component } from "react";
+import { Order } from "../Helpers/Interfaces/OrderInterfaces";
+import orderData from "../Helpers/Data/orderData";
+import { OrderCard } from "../Components/Cards/OrderCard";
+import { Table } from "reactstrap";
 
 class Orders extends Component {
-    state = {
-        orders: [],
-    };
+  state = {
+    orders: [],
+  };
 
-    componentDidMount(): void {
-        orderData.getAllUserOrders(4).then((response: Order[]) => {
-            this.setState({
-                orders: response
-            })
-        });
-    }
-    render() : JSX.Element {
-        const { orders } = this.state
-        const orderCard = (order: Order): JSX.Element => {
-            return (<OrderCard order={order}/>)
-        }
-        const cards = orders.map(orderCard)
-        return (
-            <div>
-                {cards}
-            </div>
-        )
-    }
+  componentDidMount(): void {
+    orderData.getAllUserOrders(4).then((response: Order[]) => {
+      this.setState({
+        orders: response,
+      });
+    });
+  }
+  render(): JSX.Element {
+    const { orders } = this.state;
+    const orderCard = (order: Order): JSX.Element => {
+      return <OrderCard order={order} />;
+    };
+    const cards = orders.map(orderCard);
+    return (
+      <div className="d-flex">
+        <Table hover>
+          <thead className="justify-content-center">
+            <tr>
+              <th scope="row"></th>
+              <th>Order Date</th>
+              <th>Address</th>
+              <th>Total Cost</th>
+            </tr>
+          </thead>
+          {cards}
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default Orders;
