ae1b4e7d5e35b16c05f270340b0204a36a95f4b9
Sam Mudick <smudick@gmail.com>
2021-04-29 19:20:03 -0500

    working on order table

--- spinnovations.ui/src/Components/Cards/OrderCard.tsx
+++ spinnovations.ui/src/Components/Cards/OrderCard.tsx
@@ -9,7 +9,7 @@ const calcOrderTotal = ({ order }: Order): number => {
     return totalCost;
 }
 
-export const OrderCard = ({ order }: OrderProps): JSX.Element => 
+ => 
     <div className="order-card" key={Math.random()+order.id}>
         {console.log(order)}
         <p>ORDER DATE: {order.order_Date}</p>
@@ -17,3 +17,44 @@ export const OrderCard = ({ order }: OrderProps): JSX.Element =>
         <p>ADDRESS: {order.address}, {order.city} {order.country} {order.postal_code}</p>
         <p>PRODUCT: {order.products[0].name}</p>
     </div>
+
+
+import React from 'react';
+import { Table } from 'reactstrap';
+
+export const OrderCard = ({ order }: OrderProps): JSX.Element => {
+  return (
+    <Table hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Order Date</th>
+          <th>Price</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default Example;
