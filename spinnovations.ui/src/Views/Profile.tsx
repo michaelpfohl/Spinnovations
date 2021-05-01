0ea81c3ae59d4d140d7b418a0d5558f653a99b81
Bailey <bailey.elise.dennis@gmail.com>
2021-05-01 12:02:46 -0500

    pausing for merge

--- spinnovations.ui/src/Views/Profile.tsx
+++ spinnovations.ui/src/Views/Profile.tsx
@@ -2,50 +2,61 @@ import React from 'react';
 import { User } from '../Helpers/Interfaces/UserInterfaces';
 
 type UserProps = {
-  user: User | null;
+  user: User;
 };
 class Profile extends React.Component<UserProps> {
+  state = {
+    user: null,
+  };
 
+  componentDidMount () : void {
+    this.setState({
+      user: this.props.user
+    })
+  }
 
   render(): JSX.Element {
-    const { user } = this.props;
+    const { user } = this.state;
     console.warn(user);
+    console.warn(this.props.user)
     return (
       <div id="profilePage">
-        <div className="profileCard">
-          <div className="editBtn">
-            <a href="/">
-              <i className="fas fa-pencil-alt">...</i>
+      {user != null &&
+      <div className="profileCard">
+        <div className="editBtn">
+          <a href="/">
+            <i className="fas fa-pencil-alt">...</i>
+          </a>
+        </div>
+        <img src="{user.profile_Picture}" className="profilePhoto" />
+        <div>
+          <h3 className="userName">
+            {user.Display_Name}
+          </h3>
+          <div className="buttonGroup">
+            <a href="/" id="acctinfo">
+              Account Information
+            </a>
+            <a href="/" id="myorders">
+              My Orders
+            </a>
+            <a href="/" id="submitinnovation">
+              Submit an innovation
             </a>
           </div>
-          <img src="{user.profile_Picture}" className="profilePhoto" />
-          <div>
-            <h3 className="userName">
-              {user.first_Name} {user.last_Name}
-            </h3>
-            <div className="buttonGroup">
-              <a href="/" id="acctinfo">
-                Account Information
-              </a>
-              <a href="/" id="myorders">
-                My Orders
-              </a>
-              <a href="/" id="submitinnovation">
-                Submit an innovation
-              </a>
-            </div>
-            <div className="buttonGroupTwo">
-              <a href="/">My Payments</a>
-              <a href="/">My Spins</a>
-              <a href="/">Help</a>
-            </div>
-          </div>
-          <div className="tailInfo">
-            <h5>©2021 Spinnovations inc. 100 Spin Street, Suite 400</h5>
-            <h5>Nashville, TN 37027</h5>
+          <div className="buttonGroupTwo">
+            <a href="/">My Payments</a>
+            <a href="/">My Spins</a>
+            <a href="/">Help</a>
           </div>
         </div>
+        <div className="tailInfo">
+          <h5>©2021 Spinnovations inc. 100 Spin Street, Suite 400</h5>
+          <h5>Nashville, TN 37027</h5>
+        </div>
       </div>
+   }
+   </div>
     );
   }
 }
