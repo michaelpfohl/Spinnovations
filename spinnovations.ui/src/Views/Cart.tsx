ba4ea1617679eb27a7fa8c005fe988b98ef9f8cf
Bailey <bailey.elise.dennis@gmail.com>
2021-05-04 21:15:43 -0500

    fixed the issue, waiting for merge

--- spinnovations.ui/src/Views/Cart.tsx
+++ spinnovations.ui/src/Views/Cart.tsx
@@ -19,23 +19,29 @@ class Cart extends React.Component<UserProps, cartState> {
   deleteFromCart = (productKey: string): void => {
     localStorage.removeItem(`${productKey}`);
     this.getTheCart();
-  }
+  };
 
   getTheCart = (): void => {
     const keys = Object.keys(localStorage);
     const items: Product[] = [];
-    for (const key of keys) {
-      const getCartItems = async (): Promise<Product> => {
-        const cartItem = await JSON.parse(localStorage.getItem(key) || '');
-        items.push(cartItem);
-      };
-      getCartItems().then(() => {
-        this.setState({
-          products: items,
+    if (localStorage.length) {
+      for (const key of keys) {
+        const getCartItems = async (): Promise<Product> => {
+          const cartItem = await JSON.parse(localStorage.getItem(key) || '');
+          items.push(cartItem);
+        };
+        getCartItems().then(() => {
+          this.setState({
+            products: items,
+          });
         });
+      }
+    } else {
+      this.setState({
+        products: [],
       });
     }
-  }
+  };
 
   componentDidMount(): void {
     this.getTheCart();
@@ -44,31 +50,35 @@ class Cart extends React.Component<UserProps, cartState> {
   render(): JSX.Element {
     const { user } = this.props;
     const { products } = this.state;
-    
+
     const renderProducts = () =>
       products?.map((product: Product) => (
-        <CartCard key={product.id} product={product} deleteFromCart={this.deleteFromCart} />
+        <CartCard
+          key={product.id}
+          product={product}
+          deleteFromCart={this.deleteFromCart}
+        />
       ));
     return (
       <>
-      {products?.length === 0 ? (
-        <h1>No Products</h1>
-      ) : (
-        <div id="cartPage">
-        {user != null && (
-          <div className="profileCard">
-            <div>
-              <h3 className="userName">
-                Hello, {user.display_Name}! Welcome to your Cart!
-              </h3>
+        {products?.length === 0 ? (
+          <h1>No Products</h1>
+        ) : (
+          <div id="cartPage">
+            {user != null && (
+              <div className="profileCard">
+                <div>
+                  <h3 className="userName">
+                    Hello, {user.display_Name}! Welcome to your Cart!
+                  </h3>
+                </div>
+              </div>
+            )}
+            <div className="container d-flex flex-wrap justify-content-around">
+              {renderProducts()}
             </div>
           </div>
         )}
-        <div className="container d-flex flex-wrap justify-content-around">
-          {renderProducts()}
-        </div>
-      </div>
-      )}
       </>
     );
   }
